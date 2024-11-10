66e77e6e3f73ae00ee17c7d3a4397e137fab405a
mtissh <mt.isshie@gmail.com>
2024-11-11 00:16:30 +0900

    [update]共通レイアウトを更新

--- src/lib/newt.ts
+++ src/lib/newt.ts
@@ -1,9 +1,36 @@
 import { createClient } from 'newt-client-js'
 
+interface tag {
+  _id: string
+  name: string
+  slug: string
+}
+
 export interface Article {
   title: string
   slug: string
   body: string
+  coverImage: {
+    _id: string
+    altText: string
+    description: string
+    fileName: string
+    fileType: string // TODO: 詳細に定義する
+    height: number
+    metadata: any // TODO: 型を確認
+    src: string
+    title: string
+    width: number
+  }
+  _sys: {
+    raw: {
+      createdAt: Date
+      updatedAt: Date
+      firstPublishedAt: Date
+      publishedAt: Date
+    }
+  }
+  tags: tag[]
 }
 
 export const newtClient = createClient({
